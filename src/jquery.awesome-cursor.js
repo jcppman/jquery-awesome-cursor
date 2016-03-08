afd235b132a8536722eaa3186b5f49283ffae35c
Chriest Yu <jcppman@gmail.com>
2016-03-08 17:34:39 +0800

    new supported option: options.font.size

--- src/jquery.awesome-cursor.js
+++ src/jquery.awesome-cursor.js
@@ -123,6 +123,10 @@
       options.size = typeof options.size === 'string' ?
           parseInt(options.size, 10) : options.size;
 
+      options.font.size = typeof options.font.size === 'undefined' ?
+          options.size : typeof options.font.size === 'string' ?
+              parseInt(options.font.size, 10) : options.font.size;
+
       if (typeof options.hotspot === 'string') {
         options.hotspot = parseHotspotString(options.hotspot, options.size);
       }
@@ -203,7 +207,7 @@
 
       // Draw the cursor to the canvas
       context.fillStyle = options.color;
-      context.font = options.size + 'px ' + options.font.family;
+      context.font = options.font.size + 'px ' + options.font.family;
       context.textAlign = 'center';
       context.textBaseline = 'middle';
       context.fillText(unicode, canvasSize / 2, canvasSize / 2);
